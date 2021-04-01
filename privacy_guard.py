c6d055512119fc36ff53c5edbaa5d9750586896a
Edi Kurniawan <koerniawanedi@gmail.com>
2021-04-02 00:53:08 +0700

    update face recognition

--- privacy_guard.py
+++ privacy_guard.py
@@ -15,8 +15,8 @@ def detect_face(frame, faceNet):
 	# mendefinisikan ukuran frame dan buat kedalam tipe blob
 
 	(h, w) = frame.shape[:2]
-	blob = cv2.dnn.blobFromImage(frame, 1.0, (224, 224), (104.0, 177.0, 123.0))
-	# blob = cv2.dnn.blobFromImage(frame, 1.0 / 127.5, (300, 300), (127.5, 127.5, 127.5), swapRB=True, crop=False)
+	# blob = cv2.dnn.blobFromImage(frame, 1.0, (224, 224), (104.0, 177.0, 123.0))
+	blob = cv2.dnn.blobFromImage(frame, 1.0 / 127.5, (300, 300), (127.5, 127.5, 127.5), swapRB=True, crop=False)
 	# blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300), (127.5, 127.5, 127.5), swapRB=True, crop=False)
 
 
@@ -65,36 +65,34 @@ def detect_face(frame, faceNet):
 	return(locs, preds, faces)
 
 def predict_face():
-	for (box, pred) in zip(locs, preds):
+	# for (box, pred) in zip(locs, preds):
 		# unpack the bounding box and predictions
-		(startX, startY, endX, endY) = box
-		(edi, unknown) = pred
+	(startX, startY, endX, endY) = box
+	(edi, unknown) = pred
 
-		# determine the class label and color we'll use to draw
-		# the bounding box and text
-		label_name = "Edi" if edi > unknown else "Unknown"
-		color = (0, 255, 0) if label_name == "Edi" else (0, 0, 255)
+	# determine the class label and color we'll use to draw
+	# the bounding box and text
+	label_name = "Edi" if edi > unknown else "Unknown"
+	color = (0, 255, 0) if label_name == "Edi" else (0, 0, 255)
 
-		label = "{}: {:.2f}%".format(label_name, max(edi, unknown) * 100)
-		label_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+	label = "{}: {:.2f}%".format(label_name, max(edi, unknown) * 100)
+	label_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
 
-		cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
-		cv2.rectangle(frame, (startX, startY - 20), ((startX + label_size[0][0]) + 10, startY - 2), (255, 255, 255), cv2.FILLED)
-		cv2.putText(frame, label, (startX + 4, startY - 6), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+	cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+	cv2.rectangle(frame, (startX, startY - 20), ((startX + label_size[0][0]) + 10, startY - 2), (255, 255, 255), cv2.FILLED)
+	cv2.putText(frame, label, (startX + 4, startY - 6), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
 
-		return(label_name)
-
-
+	return(label_name)
 
 # load our serialized face detector model from disk
-prototxt_path = r"face_detector/deploy_mask.prototxt"
-weights_path = r"face_detector/res10_300x300_ssd_iter_140000.caffemodel"
+# prototxt_path = r"face_detector/deploy_mask.prototxt"
+# weights_path = r"face_detector/res10_300x300_ssd_iter_140000.caffemodel"
 
 # prototxt_path = 'MobileNet-SSD-master/deploy.prototxt'
 # weights_path = 'MobileNet-SSD-master/mobilenet_iter_73000.caffemodel'
 
-# prototxt_path = 'MobilenetSSDFace-master/models/deploy/ssd-face.prototxt'
-# weights_path = 'MobilenetSSDFace-master/models/deploy/ssd-face.caffemodel'
+prototxt_path = 'MobilenetSSDFace-master/models/deploy/ssd-face.prototxt'
+weights_path = 'MobilenetSSDFace-master/models/deploy/ssd-face.caffemodel'
 
 face_net = cv2.dnn.readNet(prototxt_path, weights_path)
 
@@ -124,92 +122,71 @@ while True:
 
 	(locs, preds, faces) = detect_face(frame, face_net)
 
-	# if len(faces) == 0:
-	# 	count = count + 1
-	# 	print("[INFO] Found {0} Faces. ".format(len(faces)), count)
-	#
-	# if len(faces) == 1:
-	# 	# for faster inference we'll make batch predictions on *all*
-	# 	# faces at the same time rather than one-by-one predictions
-	# 	# in the above `for` loop
-	# 	faces = np.array(faces, dtype="float32")
-	# 	preds = face_recog.predict(faces, batch_size=32)
-	# 	label_name = predict_face()
-	#
-	# 	def get_window_hwnd(title):
-	# 		for wnd in enum_windows():
-	# 			if title.lower() in win32gui.GetWindowText(wnd).lower():
-	# 				return wnd
-	# 		return 0
-	#
-	# 	def enum_windows():
-	# 		def callback(wnd, data):
-	# 			windows.append(wnd)
-	#
-	# 		windows = []
-	# 		win32gui.EnumWindows(callback, None)
-	# 		return windows
-	#
-	# 	window = get_window_hwnd("MobileNet-SSD")
-	#
-	# 	tup = win32gui.GetWindowPlacement(window)
-	# 	count = 0
-	# 	if label_name == "Edi" and tup[1] == win32con.SW_SHOWMINIMIZED:
-	# 		counter = counter + 1
-	# 		print("[INFO] Found {0} Faces. ".format(len(faces)), counter)
-	#
-	# if len(faces) >= 2:
-	# 	count = count + 1
-	# 	print("[INFO] Found {0} Faces. ".format(len(faces)), count)
-	# 	faces = np.array(faces, dtype="float32")
-	# 	preds = face_recog.predict(faces, batch_size=32)
-	# 	label_name = predict_face()
+	if len(faces) == 0:
+		count = count + 1
+		print("[INFO] Found {0} Faces. ".format(len(faces)), count)
 
-	# if count == 15:
-	# 	keyboard = Controller()
-	# 	keyboard.press(Key.cmd)
-	# 	keyboard.press('d')
-	# 	keyboard.release('d')
-	# 	keyboard.release(Key.cmd)
-	# 	# count = 0
-	# 	print("lakukan aksi")
-	#
-	# if counter == 10:
-	# 	keyboard = Controller()
-	# 	keyboard.press(Key.cmd)
-	# 	keyboard.press('d')
-	# 	keyboard.release('d')
-	# 	keyboard.release(Key.cmd)
-	# 	counter = 0
+	if len(faces) == 1:
+		# for faster inference we'll make batch predictions on *all*
+		# faces at the same time rather than one-by-one predictions
+		# in the above `for` loop
+		faces = np.array(faces, dtype="float32")
+		preds = face_recog.predict(faces, batch_size=32)
+		for (box, pred) in zip(locs, preds):
+			label_name = predict_face()
 
-	faces = np.array(faces, dtype="float32")
+		def get_window_hwnd(title):
+			for wnd in enum_windows():
+				if title.lower() in win32gui.GetWindowText(wnd).lower():
+					return wnd
+			return 0
+
+		def enum_windows():
+			def callback(wnd, data):
+				windows.append(wnd)
+
+			windows = []
+			win32gui.EnumWindows(callback, None)
+			return windows
+
+		window = get_window_hwnd("MobileNet-SSD")
+
+		tup = win32gui.GetWindowPlacement(window)
+		count = 0
+		if label_name == "Edi" and tup[1] == win32con.SW_SHOWMINIMIZED:
+			counter = counter + 1
+			print("[INFO] Found {0} Faces. ".format(len(faces)), counter)
+
+	if len(faces) >= 2:
+		count = count + 1
+		print("[INFO] Found {0} Faces. ".format(len(faces)), count)
+		faces = np.array(faces, dtype="float32")
+		preds = face_recog.predict(faces, batch_size=32)
+		label_name = predict_face()
+
+	if count == 15:
+		keyboard = Controller()
+		keyboard.press(Key.cmd)
+		keyboard.press('d')
+		keyboard.release('d')
+		keyboard.release(Key.cmd)
+		# count = 0
+		print("lakukan aksi")
+
+	if counter == 10:
+		keyboard = Controller()
+		keyboard.press(Key.cmd)
+		keyboard.press('d')
+		keyboard.release('d')
+		keyboard.release(Key.cmd)
+		counter = 0
+
+	# faces = np.array(faces, dtype="float32")
+	# # preds = face_recog.predict(faces, batch_size=32)
 	# preds = face_recog.predict(faces, batch_size=32)
-	preds = face_recog.predict(faces, batch_size=32)
-
-
 
 	for (box, pred) in zip(locs, preds):
-		# unpack the bounding box and predictions
-		(startX, startY, endX, endY) = box
-		(edi, unknown) = pred
-
-		# determine the class label and color we'll use to draw
-		# the bounding box and text
-		label_name = "Edi" if edi > unknown else "Unknown"
-		color = (0, 255, 0) if label_name == "Edi" else (0, 0, 255)
-
-		label = "{}: {:.2f}%".format(label_name, max(edi, unknown) * 100)
-		label_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
-
-
-
-
-		cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
-		cv2.rectangle(frame, (startX, startY - 20), ((startX + label_size[0][0]) + 10, startY - 2), (255, 255, 255), cv2.FILLED)
-		cv2.putText(frame, label, (startX + 4, startY - 6), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
-
-
-		# return(label_name)
+		predict_face()
 
 	# show the output frame
 	new_frame_time = time.time()
