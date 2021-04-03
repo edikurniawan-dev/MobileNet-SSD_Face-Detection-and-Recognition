b5294acdf37d9aeeb8cb2af3b0c31e8839eda259
Edi Kurniawan <koerniawanedi@gmail.com>
2021-04-03 22:50:27 +0700

    update cropping face

--- mobnet_ssd_detect_face.py
+++ mobnet_ssd_detect_face.py
@@ -1,19 +1,17 @@
 import os
 import cv2
 import numpy as np
+from imutils.video import VideoStream
 
-# Define paths
-# base_dir = os.path.dirname(__file__)
-# prototxt_path = 'face-detector-model/other-model-face-detector.prototxt'
-# caffemodel_path = 'face-detector-model/res10_300x300_ssd_iter_140000.caffemodel'
+prototxt_path = 'face-detector-model/ssd-model/deploy.prototxt'
+caffemodel_path = 'face-detector-model/ssd-model/res10_300x300_ssd_iter_140000.caffemodel'
 
 # prototxt_path = 'MobilenetSSDFace-master/mobnet-ssd-model/deploy/ssd-face.prototxt'
 # caffemodel_path = 'MobilenetSSDFace-master/mobnet-ssd-model/deploy/ssd-face.caffemodel'
 
-prototxt_path = 'MobilenetSSDFace-master/models/deploy/ssd-face.prototxt'
-caffemodel_path = 'MobilenetSSDFace-master/models/deploy/ssd-face.caffemodel'
+# prototxt_path = 'MobilenetSSDFace-master/models/deploy/ssd-face.prototxt'
+# caffemodel_path = 'MobilenetSSDFace-master/models/deploy/ssd-face.caffemodel'
 
-# Read the model
 model = cv2.dnn.readNetFromCaffe(prototxt_path, caffemodel_path)
 
 # load all image in folder
@@ -22,38 +20,60 @@ model = cv2.dnn.readNetFromCaffe(prototxt_path, caffemodel_path)
 #     if (file_extension in ['.png','.jpg']):
 #         print("Image path: {}".format(base_dir + 'images/' + file))
 
+vs = VideoStream(src=0).start()
 
-image = cv2.imread("elif.jpg")
-(h, w) = image.shape[:2]
-# blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
-blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0 / 127.5, (300, 300), (127.5, 127.5, 127.5), swapRB=True, crop=False)
+def crop_face(count):
+    # Create frame around face
+    # count = count + 1
+    image = cv2.imread("dataset/edi/image_capture.jpg")
+    (h, w) = image.shape[:2]
+    blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
+    # blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0 / 127.5, (300, 300), (127.5, 127.5, 127.5), swapRB=True, crop=False)
 
-model.setInput(blob)
-detections = model.forward()
+    model.setInput(blob)
+    detections = model.forward()
 
-# Create frame around face
-for i in range(0, detections.shape[2]):
-    box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-    (startX, startY, endX, endY) = box.astype("int")
+    for i in range(0, detections.shape[2]):
+        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+        (startX, startY, endX, endY) = box.astype("int")
 
-    confidence = detections[0, 0, i, 2]
+        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+        (startX, startY, endX, endY) = box.astype("int")
+        confidence = detections[0, 0, i, 2]
 
-    box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-    (startX, startY, endX, endY) = box.astype("int")
-    confidence = detections[0, 0, i, 2]
+      # If confidence > 0.5, show box around face
+        if (confidence > 0.5):
+            cv2.rectangle(image, (startX, startY), (endX, endY), (255, 255, 255), 2)
+            # cv2.imwrite('dataset/edi/image_face_box.jpg', image)
+            frame = image[startY:endY, startX:endX]
+            cv2.imwrite('dataset/edi/face' + str(count) + '.jpg', frame)
 
-  # If confidence > 0.5, show box around face
-    if (confidence > 0.5):
-        cv2.rectangle(image, (startX, startY), (endX, endY), (255, 255, 255), 2)
-        frame = image[startY:endY, startX:endX]
-        cv2.imwrite('face' + str(i) + '.jpg', frame)
-        # cv2.imwrite('image.jpg', image)
+        os.remove("dataset/edi/image_capture.jpg")
 
-print("Image converted successfully")
+    print("Image cropped successfully")
 
-# Identify each face
-# for i in range(0, detections.shape[2]):
-#
-#
-#     if (confidence > 0.5):
-        # count += 1
+count = 0
+while True:
+    # grab the frame from the threaded video stream and resize it
+    # to have a maximum width of 400 pixels
+    frame = vs.read()
+    flip_frame = cv2.flip(frame, 1)
+    # frame = imutils.resize(frame, width=400)
+    # show the output frame
+    cv2.imshow("Frame", flip_frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    # if the `spacebar` key was pressed, write the *original* frame to disk
+    # so we can later process it and use it for face recognition
+    if key == 32:
+        count =+ 1
+        cv2.imwrite('dataset/edi/image_capture.jpg', flip_frame)
+        print("Image captured!")
+        crop_face(count)
+
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
+
+cv2.destroyAllWindows()
+vs.stop()
